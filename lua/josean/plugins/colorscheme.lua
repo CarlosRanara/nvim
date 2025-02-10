65f4fff7a19e6b6e25336f5c2f669c6c63a00d5e
Carlos Ranara <carlosranara0@gmail.com>
2025-02-10 23:51:07 +0800

    草

--- lua/josean/plugins/colorscheme.lua
+++ lua/josean/plugins/colorscheme.lua
@@ -1,40 +1,35 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- Load before other plugins
     config = function()
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
+      local colors = require("catppuccin.palettes").get_palette("macchiato")
 
-      require("tokyonight").setup({
-        style = "night",
-        on_colors = function(colors)
-          colors.bg = bg
-          colors.bg_dark = bg_dark
-          colors.bg_float = bg_dark
-          colors.bg_highlight = bg_highlight
-          colors.bg_popup = bg_dark
-          colors.bg_search = bg_search
-          colors.bg_sidebar = bg_dark
-          colors.bg_statusline = bg_dark
-          colors.bg_visual = bg_visual
-          colors.border = border
-          colors.fg = fg
-          colors.fg_dark = fg_dark
-          colors.fg_float = fg
-          colors.fg_gutter = fg_gutter
-          colors.fg_sidebar = fg_dark
+      require("catppuccin").setup({
+        flavour = "macchiato", -- Use Macchiato theme
+        on_colors = function(c)
+          c.bg = colors.base
+          c.bg_dark = colors.crust
+          c.bg_float = colors.mantle
+          c.bg_highlight = colors.surface0
+          c.bg_popup = colors.surface1
+          c.bg_search = colors.peach
+          c.bg_sidebar = colors.mantle
+          c.bg_statusline = colors.surface0
+          c.bg_visual = colors.overlay0
+          c.border = colors.overlay1
+          c.fg = colors.text
+          c.fg_dark = colors.subtext1
+          c.fg_float = colors.text
+          c.fg_gutter = colors.overlay1
+          c.fg_sidebar = colors.subtext0
         end,
       })
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+
+      -- Apply the theme
+      vim.cmd([[colorscheme catppuccin]])
     end,
   },
 }
+

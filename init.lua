50916838d0ef6dcad0c638e34720fa4e53f77dfe
callo <callo@callos-MacBook-Air.local>
2025-05-08 08:31:02 +0800

    4 spaces per tab

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ require("josean.lazy")
 
 vim.opt.clipboard = "unnamedplus"
 
-vim.o.tabstop = 8       -- A tab is displayed as 8 spaces
-vim.o.shiftwidth = 8    -- Indentation is 8 spaces
+vim.o.tabstop = 4       -- A tab is displayed as 8 spaces
+vim.o.shiftwidth = 4    -- Indentation is 8 spaces
 vim.o.expandtab = false -- Use actual tab characters instead of spaces
 vim.o.autoindent = true -- Maintain indentation level of the previous line
 vim.o.cindent = true    -- Enable C-style indentation
@@ -15,8 +15,8 @@ vim.opt.cinoptions = ":0,l1,t0,+4,(0,u0,w1" -- Specific indentation rules for C
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "c", "cpp" },
     callback = function()
-        vim.opt_local.tabstop = 8
-        vim.opt_local.shiftwidth = 8
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
         vim.opt_local.expandtab = false
     end,
 })
